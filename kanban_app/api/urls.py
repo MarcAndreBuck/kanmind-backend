8cc908388cbbf3df280464fa3223f947c7b1a4f6
Marc-Andre Buck <marcbuck94@gmail.com>
2026-04-01 23:04:08 +0200

    feat: add board routing

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,3 +1,8 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from kanban_app.api.views import BoardViewSet
+
+router = DefaultRouter()
+router.register("boards", BoardViewSet, basename="board")
+
+urlpatterns = router.urls
