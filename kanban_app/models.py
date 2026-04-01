80d1e90034909d2657ca6438c009df41f94244ea
Marc-Andre Buck <marcbuck94@gmail.com>
2026-04-01 23:00:50 +0200

    feat: add board model

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="boards",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
