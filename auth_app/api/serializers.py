28c8ba71d7a2313e4b240583baead0219163373e
Marc-Andre Buck <marcbuck94@gmail.com>
2026-04-02 23:02:10 +0200

    feat: add registration serializer

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from auth_app.models import UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["user", "fullname"]
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    fullname = serializers.CharField(write_only=True)
+    repeated_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["fullname", "email", "password", "repeated_password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["repeated_password"]:
+            raise serializers.ValidationError("Passwords don't match")
+        return attrs
+
+    def save(self,  **kwargs):
+        account = User(
+            email=self.validated_data["email"], username=self.validated_data["email"])
+
+        account.set_password(self.validated_data["password"])
+        account.save()
+
+        UserProfile.objects.create(
+            user=account,
+            fullname=self.validated_data["fullname"]
+        )
+
+        return account
