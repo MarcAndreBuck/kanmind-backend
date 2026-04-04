ef0561f0988ed322d05f8875fe13fec4afffb36f
Marc-Andre Buck <marcbuck94@gmail.com>
2026-04-04 22:13:18 +0200

    feat(boards): add serializers for list, detail and update responses

--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -1,16 +1,35 @@
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from kanban_app.api.serializers import BoardSerializer
+from kanban_app.api.serializers import (
+    BoardListSerializer,
+    BoardRetrieveSerializer,
+    BoardUpdateSerializer,
+)
 from kanban_app.models import Board
 
 
 class BoardViewSet(ModelViewSet):
-    serializer_class = BoardSerializer
+    queryset = Board.objects.all()
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Board.objects.filter(owner=self.request.user)
+        user = self.request.user
+
+        if user.is_staff:
+            return Board.objects.all()
+
+        return Board.objects.filter(
+            Q(owner=user) | Q(members=user)
+        ).distinct()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BoardRetrieveSerializer
+        if self.action == "partial_update":
+            return BoardUpdateSerializer 
+        return BoardListSerializer
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        board = serializer.save(owner=self.request.user)
