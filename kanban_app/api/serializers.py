4ce984ce07d6e6ba9bfd9ab86824335c7c27b382
Marc-Andre Buck <marcbuck94@gmail.com>
2026-04-04 22:12:34 +0200

    feat(boards): add serializers for list, detail and update responses

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -1,9 +1,120 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from kanban_app.models import Board
 
-class BoardSerializer(serializers.ModelSerializer):
-    owner_id = serializers.IntegerField(source = "owner.id", read_only=True)
+
+class BoardMemberSerializer(serializers.ModelSerializer):
+    fullname = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "fullname"]
+
+    def get_fullname(self, obj):
+        return obj.profile.fullname
+
+
+class BoardListSerializer(serializers.ModelSerializer):
+    owner_id = serializers.IntegerField(source="owner.id", read_only=True)
+    members = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all(),
+        required=False,
+    )
+    member_count = serializers.SerializerMethodField()
+    ticket_count = serializers.SerializerMethodField()
+    tasks_to_do_count = serializers.SerializerMethodField()
+    tasks_high_prio_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Board
-        fields = ["id", "title", "owner_id"]
+        fields = [
+            "id",
+            "title",
+            "members",
+            "member_count",
+            "ticket_count",
+            "tasks_to_do_count",
+            "tasks_high_prio_count",
+            "owner_id",
+        ]
+
+    def get_member_count(self, obj):
+        return obj.members.count()
+
+    def get_ticket_count(self, obj):
+        return 0
+
+    def get_tasks_to_do_count(self, obj):
+        return 0
+
+    def get_tasks_high_prio_count(self, obj):
+        return 0
+
+
+class BoardRetrieveSerializer(serializers.ModelSerializer):
+    owner_id = serializers.IntegerField(source="owner.id", read_only=True)
+    members = BoardMemberSerializer(many=True, read_only=True)
+    tasks = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Board
+        fields = [
+            "id",
+            "title",
+            "owner_id",
+            "members",
+            "tasks",
+        ]
+
+    def get_tasks(self, obj):
+        return []
+
+
+class BoardUpdateSerializer(serializers.ModelSerializer):
+    members = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all(),
+        required=False,
+        write_only=True,
+    )
+    owner_data = serializers.SerializerMethodField()
+    members_data = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Board
+        fields = [
+            "id",
+            "title",
+            "members",
+            "owner_data",
+            "members_data",
+        ]
+
+    def get_owner_data(self, obj):
+        return {
+            "id": obj.owner.id,
+            "email": obj.owner.email,
+            "fullname": obj.owner.profile.fullname,
+        }
+
+    def get_members_data(self, obj):
+        return [
+            {
+                "id": user.id,
+                "email": user.email,
+                "fullname": user.profile.fullname,
+            }
+            for user in obj.members.all()
+        ]
+
+    def update(self, instance, validated_data):
+        members = validated_data.pop("members", None)
+
+        instance.title = validated_data.get("title", instance.title)
+        instance.save()
+
+        if members is not None:
+            instance.members.set(members)
+
+        return instance
