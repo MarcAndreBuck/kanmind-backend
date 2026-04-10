fb560c222154878963f8e035fb38d263ad9579cc
Marc-Andre Buck <marcbuck94@gmail.com>
2026-04-10 10:25:52 +0200

    feat(admin): register models for admin interface
    - add UserProfile admin configuration
    - register Board, Task and Comment models

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Board, Task, Comment
 
-# Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner")
+    search_fields = ("title", "owner__email")
+    filter_horizontal = ("members",)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "board", "status", "priority", "assignee", "reviewer")
+    list_filter = ("status", "priority")
+    search_fields = ("title", "description")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "task", "author", "created_at")
+    search_fields = ("task__title", "author__email")
+
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment, CommentAdmin)
